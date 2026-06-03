44bb9fd43a73dbedeb3db1dd8e2c1ec88059af88
unknown <borna.skunca@sunturist.com>
2026-06-03 12:57:24 +0200

    fix izleti

--- frontend/src/services/izletService.js
+++
@@ -1,9 +0,0 @@
-import api from "../api/axios";
-
-export const getIzleti=()=>api.get("/izleti");
-
-export const createIzlet=(data)=>api.post("/izleti",data);
-
-export const updateIzlet=(id,data)=>api.put(`/izleti/${id}`,data);
-
-export const deleteIzlet=(id)=>api.delete(`/izleti/${id}`);
